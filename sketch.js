660df2afec0c445ae353415b28eb5f2734a44c24
Anthea777 <78027677+Anthea777@users.noreply.github.com>
2021-11-14 22:25:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
